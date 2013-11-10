1643292d8db5adf30bd92f17b7fa6fe0fe6c78d5
mithilarun <arun.mithil@gmail.com>
2013-11-10 13:42:02 +0530

    Added extra screens, fixed image sizes

--- gen/com/orderthis/heywaiter/R.java
+++ gen/com/orderthis/heywaiter/R.java
@@ -21,35 +21,73 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int applepie=0x7f020000;
+        public static final int basiltomatopasta=0x7f020001;
+        public static final int bbqchickenwings=0x7f020002;
+        public static final int bg=0x7f020003;
+        public static final int caramelcustard=0x7f020004;
+        public static final int cheeserolls=0x7f020005;
+        public static final int chocmoussecake=0x7f020006;
+        public static final int eggsaladsandwch=0x7f020007;
+        public static final int fettuccinialfredo=0x7f020008;
+        public static final int fishstick=0x7f020009;
+        public static final int hamburgersteak=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
+        public static final int onionrings=0x7f02000c;
+        public static final int rasptriffle=0x7f02000d;
+        public static final int spicelambburger=0x7f02000e;
+        public static final int spinchmushrmlasagna=0x7f02000f;
+        public static final int vegquiche=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080006;
-        public static final int button2=0x7f080005;
+        public static final int action_settings=0x7f08000b;
+        public static final int button1=0x7f080005;
+        public static final int button2=0x7f08000a;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080001;
         public static final int tableRow3=0x7f080002;
         public static final int tableRow4=0x7f080003;
         public static final int tableRow5=0x7f080004;
+        public static final int tableRow6=0x7f080007;
+        public static final int tableRow7=0x7f080008;
+        public static final int tableRow8=0x7f080009;
+        public static final int test=0x7f080006;
     }
     public static final class layout {
-        public static final int activity_main_course=0x7f030000;
-        public static final int activity_starters=0x7f030001;
-        public static final int activity_welcome_page=0x7f030002;
+        public static final int activity_desserts=0x7f030000;
+        public static final int activity_main_course=0x7f030001;
+        public static final int activity_starters=0x7f030002;
+        public static final int activity_welcome_page=0x7f030003;
     }
     public static final class menu {
-        public static final int main_course=0x7f070000;
-        public static final int starters=0x7f070001;
-        public static final int welcome_page=0x7f070002;
+        public static final int desserts=0x7f070000;
+        public static final int main_course=0x7f070001;
+        public static final int starters=0x7f070002;
+        public static final int welcome_page=0x7f070003;
     }
     public static final class string {
+        public static final int BBQ=0x7f050016;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int applePie=0x7f05000b;
+        public static final int basilTomato=0x7f050013;
+        public static final int caramelCustard=0x7f05000a;
+        public static final int cheeseRolls=0x7f050014;
+        public static final int chocMousse=0x7f05000c;
+        public static final int confirm=0x7f050018;
+        public static final int countryQuiche=0x7f05000f;
+        public static final int eggSaladSandwich=0x7f05000d;
+        public static final int fettuccineAlfredo=0x7f050012;
+        public static final int fishSticks=0x7f050017;
+        public static final int hamburgerSteak=0x7f050011;
         public static final int hello_world=0x7f050002;
+        public static final int lambBurger=0x7f050010;
         public static final int maincourse=0x7f050006;
+        public static final int onionRings=0x7f050015;
+        public static final int raspTrif=0x7f050009;
+        public static final int spinachMushroom=0x7f05000e;
         public static final int starter=0x7f050004;
+        public static final int title_activity_desserts=0x7f050008;
         public static final int title_activity_main_course=0x7f050005;
         public static final int title_activity_starters=0x7f050003;
         public static final int welcome=0x7f050007;
--- src/com/orderthis/heywaiter/MainCourse.java
+++ src/com/orderthis/heywaiter/MainCourse.java
@@ -2,7 +2,10 @@ package com.orderthis.heywaiter;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainCourse extends Activity {
 
@@ -10,6 +13,17 @@ public class MainCourse extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_course);
+		
+		Button b1 = (Button) findViewById (R.id.button1);
+		
+		b1.setOnClickListener(new View.OnClickListener(){
+       	 public void onClick(View v){
+       		 //Context context = getApplicationContext();
+       		 
+       		 Intent intent = new Intent(getApplicationContext(), Desserts.class);
+      	    	 startActivity(intent);
+       	 }
+       });
 	}
 
 	@Override
