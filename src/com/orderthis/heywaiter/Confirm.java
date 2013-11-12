94ac9ab4fce31948a2b7f8cace261fa021ffb90c
mithilarun <arun.mithil@gmail.com>
2013-11-12 11:46:11 +0530

    All pages linked. DB class added.

--- src/com/orderthis/heywaiter/Confirm.java
+++ src/com/orderthis/heywaiter/Confirm.java
@@ -1,7 +1,10 @@
 package com.orderthis.heywaiter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.view.ViewGroup.LayoutParams;
@@ -17,15 +20,25 @@ public class Confirm extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
 
+        Button b1 = (Button) findViewById (R.id.button1);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Context context = getApplicationContext();
+
+                Intent intent = new Intent(getApplicationContext(), ThankYou.class);
+                startActivity(intent);
+            }
+        });
         TableLayout layout=(TableLayout)findViewById(R.id.tablelayout);
-        while(/*something*/layout!=null/*sample condition*/)
+        /*while(/*somethinglayout!=null/*sample condition)
         {
             TableRow tr=new TableRow(this);
             tr.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT));
             TextView textview = new TextView(this);
-            textview.setText("data"/*sample*/);
+            textview.setText("data"/*sample);
             tr.addView(textview);
             layout.addView(tr);
-        }
+        }*/
     }
 }
